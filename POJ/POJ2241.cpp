dfb121db657ce173cb2e2c0eb35d6221775caed5
Inv <Inv>
2017-03-05 16:13:24 +0800

    all

--- POJ/POJ2241.cpp
+++ POJ/POJ2241.cpp
@@ -1,30 +1,31 @@
-#include<iostream>
+#include<iostream>//Accepted!
 #include<cstdio>
 #include<cstdlib>
 #include<cstring>
 #include<algorithm>
-#define N 10010
+#define N 200
 using namespace std;
 struct blocks{
     int a,b,h;
-    bool operator <(const blocks &x){
-        if(this->a<x.a && this->b<x.b)return true;
-        else return false;
-    }
 }a[N];
-int top=0,n,f[N*6];
-inline void addup(int x,int y,int z){
+int top=0,n,f[N];
+void addup(int x,int y,int z){
     a[++top].a=x,a[top].b=y,a[top].h=z;
 }
 inline bool cmp(blocks x,blocks y){
-    if(x.a==y.a)return x.b>y.b;
-    else return x.a>y.a;
+    if(x.a==y.a)return x.b<y.b;
+    else return x.a<y.a;
+}
+void clear(){
+    memset(f,0,sizeof(f));
+    top=0;
 }
 int main(){
     int cnt=0;
     while(scanf("%d",&n) && n!=0){
         cnt++;
         int ans=0;
+        clear();
         for(int i=1;i<=n;i++){
             int x,y,z;
             scanf("%d%d%d",&x,&y,&z);
@@ -35,13 +36,13 @@ int main(){
             addup(z,x,y);
             addup(z,y,x);
         }
-        sort(a+1,a+1+n,cmp);
-        for(int i=1;i<=top;i++){
+        sort(a+1,a+1+top,cmp);
+        for(int i=1;i<=n;i++){
             f[i]=a[i].h;
         }
-        for(int i=1;i<=top;i++){
-            for(int j=i+1;j<=top;j++){
-                if(a[j]<a[i])f[i]=max(f[i],f[j]+a[j].h);
+        for(int i=2;i<=top;i++){
+            for(int j=i-1;j>=1;j--){
+                if(a[j].a<a[i].a && a[j].b<a[i].b)f[i]=max(f[i],f[j]+a[i].h);
             }
         }
         for(int i=1;i<=top;i++){
